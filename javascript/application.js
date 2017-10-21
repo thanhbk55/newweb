61e12f90ff94f9059bf2f7119a19acaf10b90fc2
thanhbk55 <dinhduythanh51292@gmail.com>
2017-10-21 16:58:13 +0900

    new web

---
+++ javascript/application.js
@@ -0,0 +1,161 @@
+
+
+var gradient_list = {
+  name: "Wedding Day Blues@Sand to Blue@Quepal@Pun Yeta@Sublime Light@Sublime Vivid@Bighead@Taran Tado@Relaxing red@Lawrencium@Ohhappiness@Delicate@Selenium@Sulphur@Pink Flavour@Orange Fun@Velvet Sun@Digital Water@Lithium@Argon@Hydrogen@Zinc@Velvet Sun@King Yna@Summer@Orange Coral@Purpink@Dull@Kimoby Is The New Blue@Broken Hearts@Clot@Shift@Subu@Socialive@Crimson Tide@Telegram@Terminal@Scooter@Alive@Relay@Meridian@Compare Now@Mello@Crystal Clear@Visions of Grandeur@Chitty Chitty Bang Bang@Blue Skies@Sunkist@Coal@Html@Cinnamint@Maldives@Mini@Sha la la@Purplepine@Celestial@Learning and Leading@Pacific Dream@Venice@Orca@Love and Liberty@Very Blue@Can You Feel The Love Tonight@The Blue Lagoon@Under the Lake@Honey Dew@Roseanna@What lies Beyond@Rose Colored Lenses@EasyMed@Cocoaa Ice@Jaipur@Vice City@Mild@Dawn@Ibiza Sunset@Radar@80's Purple@Black Rosé@Brady Brady Fun Fun@Ed's Sunset Gradient@Snapchat@Cosmic Fusion@Nepal@Azure Pop@Love Couple@Disco@Limeade@Dania@50 Shades of Grey@Dusk@IIIT Delhi@Sun on the Horizon@Blood Red@Sherbert@Firewatch@Lush@Frost@Mauve@Royal@Minimal Red@Dusk@Deep Sea Space@Grapefruit Sunset@Sunset@Solid Vault@Bright Vault@Politics@Sweet Morning@Sylvia@Transfile@Tranquil@Red Ocean@Shahabi@Alihossein@Ali@Purple White@Colors Of Sky@Decent@Deep Space@Dark Skies@Suzy@Superman@Nighthawk@Forest@Miami Dolphins@Minnesota Vikings@Christmas@Joomla@Pizelex@Haikus@Pale Wood@Purplin@Inbox@Blush@Back to the Future@Poncho@Green and Blue@Light Orange@Netflix@Little Leaf@Deep Purple@Back To Earth@Master Card@Clear Sky@Passion@Timber@Between Night and Day@Sage Persuasion@Lizard@Piglet@Dark Knight@Curiosity blue@Ukraine@Green to dark@Fresh Turboscent@Koko Caramel@Virgin America@Portrait@Turquoise flow@Vine@Flickr@Instagram@Atlas@Twitch@Pastel Orange at the Sun@Endless River@Predawn@Purple Bliss@Talking To Mice Elf@Hersheys@Crazy Orange I@Between The Clouds@Metallic Toad@Martini@Friday@ServQuick@Behongo@SoundCloud@Facebook Messenger@Shore@Cheer Up Emo Kid@Amethyst@Man of Steel@Neon Life@Teal Love@Red Mist@Starfall@Dance To Forget@Parklife@Cherryblossoms@Shadow Night@Ash@Virgin@Earthly@Dirty Fog@The Strain@Reef@Candy@Autumn@Winter@Forever Lost@Almost@Moor@Aqualicious@Misty Meadow@Kyoto@Sirius Tamed@Jonquil@Petrichor@A Lost Memory@Vasily@Blurry Beach@Namn@Day Tripper@Pinot Noir@Miaka@Army@Shrimpy@Influenza@Calm Darya@Bourbon@Stellar@Clouds@Moonrise@Peach@Dracula@Mantle@Titanium@Opa@Sea Blizz@Midnight City@Mystic@Shroom Haze@Moss@Bora Bora@Venice Blue@Electric Violet@Kashmir@Steel Gray@Mirage@Juicy Orange@Mojito@Cherry@Pinky@Sea Weed@Stripe@Purple Paradise@Sunrise@Aqua Marine@Aubergine@Bloody Mary@Mango Pulp@Frozen@Rose Water@Horizon@Monte Carlo@Lemon Twist@Emerald Water@Intuitive Purple@Green Beach@Sunny Days@Playing with Reds@Harmonic Energy@Cool Brown@YouTube@Noon to Dusk@Hazel@Nimvelo@Sea Blue@Blooker20@Sexy Blue@Purple Love@DIMIGO@Skyline@Sel@Sky@Petrol@Copper@Royal Blue + Petrol@Royal Blue@Windy@Mango",
+  color_set: "#40e0d0!#ff8c00!#ff0080!@#3e5151!#decba4!@#11998e!#38ef7d!@#108dc7!#ef8e38!@#fc5c7d!#6a82fb!@#fc466b!#3f5efb!@#c94b4b!#4b134f!@#23074d!#cc5333!@#fffbd5!#b20a2c!@#0f0c29!#302b63!#24243e!@#00b09b!#96c93d!@#d3cce3!#e9e4f0!@#3c3b3f!#605c3c!@#cac531!#f3f9a7!@#800080!#ffc0cb!@#fc4a1a!#f7b733!@#e1eec3!#f05053!@#74ebd5!#acb6e5!@#6d6027!#d3cbb8!@#03001e!#7303c0!#ec38bc!#fdeff9!@#667db6!#0082c8!#0082c8!#667db6!@#ada996!#f2f2f2!#dbdbdb!#eaeaea!@#e1eec3!#f05053!@#1a2a6c!#b21f1f!#fdbb2d!@#22c1c3!#fdbb2d!@#ff9966!#ff5e62!@#7f00ff!#e100ff!@#c9d6ff!#e2e2e2!@#396afc!#2948ff!@#d9a7c7!#fffcdc!@#070000!#4c0001!#070000!@#000000!#e5008d!#ff070b!@#0cebeb!#20e3b2!#29ffc6!@#06beb6!#48b1bf!@#642b73!#c6426e!@#1c92d2!#f2fcfe!@#000000!#0f9b0f!@#36d1dc!#5b86e5!@#cb356b!#bd3f32!@#3a1c71!#d76d77!#ffaf7b!@#283c86!#45a247!@#ef3b36!#ffffff!@#c0392b!#8e44ad!@#159957!#155799!@#000046!#1cb5e0!@#007991!#78ffd6!@#56ccf2!#2f80ed!@#f2994a!#f2c94c!@#eb5757!#000000!@#e44d26!#f16529!@#4ac29a!#bdfff3!@#b2fefa!#0ed2f7!@#30e8bf!#ff8235!@#d66d75!#e29587!@#20002c!#cbb4d4!@#c33764!#1d2671!@#f7971e!#ffd200!@#34e89e!#0f3443!@#6190e8!#a7bfe8!@#44a08d!#093637!@#200122!#6f0000!@#0575e6!#021b79!@#4568dc!#b06ab3!@#43c6ac!#191654!@#093028!#237a57!@#43c6ac!#f8ffae!@#ffafbd!#ffc3a0!@#f0f2f0!#000c40!@#e8cbc0!#636fa4!@#dce35b!#45b649!@#c0c0aa!#1cefff!@#dbe6f6!#c5796d!@#3494e6!#ec6ead!@#67b26f!#4ca2cd!@#f3904f!#3b4371!@#ee0979!#ff6a00!@#a770ef!#cf8bf3!#fdb99b!@#41295a!#2f0743!@#f4c4f3!#fc67fa!@#00c3ff!#ffff1c!@#ff7e5f!#feb47b!@#fffc00!#ffffff!@#ff00cc!#333399!@#de6161!#2657eb!@#ef32d9!#89fffd!@#3a6186!#89253e!@#4ecdc4!#556270!@#a1ffce!#faffd1!@#be93c5!#7bc6cc!@#bdc3c7!#2c3e50!@#ffd89b!#19547b!@#808080!#3fada8!@#fceabb!#f8b500!@#f85032!#e73827!@#f79d00!#64f38c!@#cb2d3e!#ef473a!@#56ab2f!#a8e063!@#000428!#004e92!@#42275a!#734b6d!@#141e30!#243b55!@#f00000!#dc281e!@#2c3e50!#fd746c!@#2c3e50!#4ca1af!@#e96443!#904e95!@#0b486b!#f56217!@#3a7bd5!#3a6073!@#00d2ff!#928dab!@#2196f3!#f44336!@#ff5f6d!#ffc371!@#ff4b1f!#ff9068!@#16bffd!#cb3066!@#eecda3!#ef629f!@#1d4350!#a43931!@#a80077!#66ff00!@#f7ff00!#db36a4!@#ff4b1f!#1fddff!@#ba5370!#f4e2d8!@#e0eafc!#cfdef3!@#4ca1af!#c4e0e5!@#000000!#434343!@#4b79a1!#283e51!@#834d9b!#d04ed6!@#0099f7!#f11712!@#2980b9!#2c3e50!@#5a3f37!#2c7744!@#4da0b0!#d39d38!@#5614b0!#dbd65c!@#2f7336!#aa3a38!@#1e3c72!#2a5298!@#114357!#f29492!@#fd746c!#ff9068!@#eacda3!#d6ae7b!@#6a3093!#a044ff!@#457fca!#5691c8!@#b24592!#f15f79!@#c02425!#f0cb35!@#403a3e!#be5869!@#c2e59c!#64b3f4!@#ffb75e!#ed8f03!@#8e0e00!#1f1c18!@#76b852!#8dc26f!@#673ab7!#512da8!@#00c9ff!#92fe9d!@#f46b45!#eea849!@#005c97!#363795!@#e53935!#e35d5b!@#fc00ff!#00dbde!@#2c3e50!#3498db!@#ccccb2!#757519!@#304352!#d7d2cc!@#ee9ca7!#ffdde1!@#ba8b02!#181818!@#525252!#3d72b4!@#004ff9!#fff94c!@#6a9113!#141517!@#f1f2b5!#135058!@#d1913c!#ffd194!@#7b4397!#dc2430!@#8e9eab!#eef2f3!@#136a8a!#267871!@#00bf8f!#001510!@#ff0084!#33001b!@#833ab4!#fd1d1d!#fcb045!@#feac5e!#c779d0!#4bc0c8!@#6441a5!#2a0845!@#ffb347!#ffcc33!@#43cea2!#185a9d!@#ffa17f!#00223e!@#360033!#0b8793!@#948e99!#2e1437!@#1e130c!#9a8478!@#d38312!#a83279!@#73c8a9!#373b44!@#abbaab!#ffffff!@#fdfc47!#24fe41!@#83a4d4!#b6fbff!@#485563!#29323c!@#52c234!#061700!@#fe8c00!#f83600!@#00c6ff!#0072ff!@#70e1f5!#ffd194!@#556270!#ff6b6b!@#9d50bb!#6e48aa!@#780206!#061161!@#b3ffab!#12fff7!@#aaffa9!#11ffbd!@#000000!#e74c3c!@#f0c27b!#4b1248!@#ff4e50!#f9d423!@#add100!#7b920a!@#fbd3e9!#bb377d!@#000000!#53346d!@#606c88!#3f4c6b!@#c9ffbf!#ffafbd!@#649173!#dbd5a4!@#b993d6!#8ca6db!@#870000!#190a05!@#00d2ff!#3a7bd5!@#d3959b!#bfe6ba!@#dad299!#b0dab9!@#e6dada!#274046!@#5d4157!#a8caba!@#ddd6f3!#faaca8!@#616161!#9bc5c3!@#50c9c3!#96deda!@#215f00!#e4e4d9!@#c21500!#ffc500!@#efefbb!#d4d3dd!@#ffeeee!#ddefbb!@#666600!#999966!@#de6262!#ffb88c!@#e9d362!#333333!@#d53369!#cbad6d!@#a73737!#7a2828!@#f857a6!#ff5858!@#4b6cb7!#182848!@#fc354c!#0abfbc!@#414d0b!#727a17!@#e43a15!#e65245!@#c04848!#480048!@#5f2c82!#49a09d!@#ec6f66!#f3a183!@#7474bf!#348ac7!@#ece9e6!#ffffff!@#dae2f8!#d6a4a4!@#ed4264!#ffedbc!@#dc2424!#4a569d!@#24c6dc!#514a9d!@#283048!#859398!@#3d7eaa!#ffe47a!@#1cd8d2!#93edc7!@#232526!#414345!@#757f9a!#d7dde8!@#5c258d!#4389a2!@#134e5e!#71b280!@#2bc0e4!#eaecc6!@#085078!#85d8ce!@#4776e6!#8e54e9!@#614385!#516395!@#1f1c2c!#928dab!@#16222a!#3a6073!@#ff8008!#ffc837!@#1d976c!#93f9b9!@#eb3349!#f45c43!@#dd5e89!#f7bb97!@#4cb8c4!#3cd3ad!@#1fa2ff!#12d8fa!#a6ffcb!@#1d2b64!#f8cdda!@#ff512f!#f09819!@#1a2980!#26d0ce!@#aa076b!#61045f!@#ff512f!#dd2476!@#f09819!#edde5d!@#403b4a!#e7e9bb!@#e55d87!#5fc3e4!@#003973!#e5e5be!@#cc95c0!#dbd4b4!#7aa1d2!@#3ca55c!#b5ac49!@#348f50!#56b4d3!@#da22ff!#9733ee!@#02aab0!#00cdac!@#ede574!#e1f5c4!@#d31027!#ea384d!@#16a085!#f4d03f!@#603813!#b29f94!@#e52d27!#b31217!@#ff6e7f!#bfe9ff!@#77a1d3!#79cbca!#e684ae!@#314755!#26a0da!@#2b5876!#4e4376!@#e65c00!#f9d423!@#2193b0!#6dd5ed!@#cc2b5e!#753a88!@#ec008c!#fc6767!@#1488cc!#2b32b2!@#00467f!#a5cc82!@#076585!#fff!@#bbd2c5!#536976!@#b79891!#94716b!@#bbd2c5!#536976!#292e49!@#536976!#292e49!@#acb6e5!#86fde8!@#ffe259!#ffa751!"
+}
+var color_name_list = gradient_list.name.split("@")
+var color_set_list = gradient_list.color_set.split("@")
+var length = color_name_list.length
+
+var index = Math.floor( Math.random() * length)
+if(index <0 || index >= length){
+  index = 0
+}
+
+Vue.config.devtools = true
+new Vue({
+  el: '#app',
+  data: () => ({
+    dialog: false,
+    drawer: false,
+    new_post: false,
+    share_speed_dial: false,
+    text_edit_mode: false,
+    app_color: "teal darken-3",
+    icon_color: "teal--text text--darken-3",
+    app_name: "My App",
+    selected_item: "Home",
+    style:{
+      button_color: "teal darken-3",
+      button_color_outline: "teal--text text--darken-3"
+    },
+    items: [
+      { icon: 'home', text: 'Home' },
+      {
+        icon: 'keyboard_arrow_up',
+        'icon-alt': 'keyboard_arrow_down',
+        text: 'Post',
+        model: false,
+        children: [
+          { text: 'New' },
+          { text: 'Edit' }
+        ]
+      },
+      { icon: 'settings', text: 'Settings' },
+      { icon: 'chat_bubble', text: 'Send feedback' },
+      { icon: 'help', text: 'Help' },
+      { icon: 'phonelink', text: 'App downloads' },
+      { icon: 'keyboard', text: 'Go to the old version' }
+    ],
+    main_items: {
+      current_select: true
+    },
+    gradient_list: {
+      color_name_list: color_name_list,
+      color_set_list: color_set_list,
+      color_index: index,
+      rotate_arr: ["to right,", "", "to left,", "to top,"],
+      rotate_index: 0
+    }
+  }),
+  created: function () {
+    that = this
+    $(document).ready(function(){
+      $(".draggable").draggable()
+      that.showActionList()
+      setTimeout(function(){
+        $("#list_action [md-fab-trigger]").click()
+      }, 500)
+    })
+
+    $(document).on("click", "[md-fab-trigger]", function(e){
+      e.currentTarget
+        .closest('.md-speed-dial')
+        .classList.toggle('md-active')
+    })
+
+    this.clickOutSide("#list_action", this.editMode)
+  },
+  watch: {
+    items(newItems){
+      console.log(newItems)
+    },
+  },
+  computed: {
+    getStyleBackground(){
+      gradient_list = this.gradient_list
+      color_set = gradient_list.color_set_list[gradient_list.color_index].split("!")
+      string_color = color_set.toString()
+      string_color = string_color.slice(0, string_color.length-1)
+      return "linear-gradient(" + gradient_list.rotate_arr[gradient_list.rotate_index] + string_color + ")"
+    },
+  },
+  methods: {
+    clickOutSide(element, callback){
+      app = $("[data-app]") || $("body")
+      $(document).on("click", app, function(e){
+        if(!$(e.target).closest(element).length){
+          callback()
+        }
+      })
+    },
+
+    showActionList(){
+      console.log("aaaa")
+    },
+    changeAppName(app_name){
+      new_post = true
+      this.selected_item = app_name
+    },
+    rotateGradien(){
+      this.gradient_list.rotate_index += 1
+      if(this.gradient_list.rotate_index >= this.gradient_list.rotate_arr.length){
+        this.gradient_list.rotate_index = 0
+      }
+    },
+    downloadImage(){
+      image_name = this.gradient_list.color_name_list[this.gradient_list.color_index]
+      clone_content = $("main").clone().empty()
+      clone_content.insertBefore("main")
+      html2canvas($("main")[0], {
+        onrendered: function(canvas) {
+          clone_content.remove()
+          var a = document.createElement('a');
+          a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+          a.download = image_name;
+          a.click();
+        }
+      });
+    },
+    changeFavicon(){
+      clone_content = $("main").clone().empty()
+      clone_content.insertBefore("main")
+      html2canvas(clone_content, {
+        onrendered: function(canvas) {
+          clone_content.remove()
+          var img_data = canvas.toDataURL("image/jpeg")
+          $("#icon").attr("href", canvas.toDataURL("image/jpeg"))
+        }
+      });
+    },
+    switchTextMode(){
+      $("#list_action [md-fab-trigger]").click()
+      that = this
+      setTimeout(function(){
+        that.text_edit_mode = !that.text_edit_mode
+        setTimeout(function(){
+          $("#list_action [md-fab-trigger]").click()
+        }, 10)
+      }, 600)
+    },
+    editMode(){
+      if(that.text_edit_mode){
+        this.switchTextMode()
+      }
+    }
+  },
+  props: {
+    source: String
+  }
+})
